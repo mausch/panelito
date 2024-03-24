9a34e365d63241f1343fe69cb44992ded2e48eeb
Mauricio Scheffer <mauricioscheffer@gmail.com>
2024-03-24 13:13:03 +0000

    Switch to flake parts

--- flake.nix
+++ flake.nix
@@ -1,48 +1,56 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # ddc-hi.url = "github:arcnmx/ddc-hi-rs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs }: 
-    let 
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages."${system}";
-    in
-  {
-
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        pkgs.mosquitto
-        pkgs.rustc
-        pkgs.cargo
-
-        # for ddc-hi
-        pkgs.udev
-        pkgs.pkg-config
-        # ddc-hi.lib.crate
-        
-        (pkgs.vscode-with-extensions.override {
-          vscode = pkgs.vscodium;
-          vscodeExtensions = (with pkgs.vscode-extensions; [
-            rust-lang.rust-analyzer
-            vadimcn.vscode-lldb
-          ]);
-        })
-      ];
-    };
-
-    packages."${system}".default =
-      pkgs.rustPlatform.buildRustPackage {
-        name = "mqtt-light";
-        src = pkgs.lib.cleanSource ./.;
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-          # Allow dependencies to be fetched from git and avoid having to set the outputHashes manually
-          allowBuiltinFetchGit = true;
-        };
-        doCheck = false;
+  outputs = inputs@{ flake-parts, nixpkgs, self }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = {
       };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      perSystem = { options, specialArgs, lib, system, config, pkgs }: 
+        let deps = [
+            # for ddc-hi
+            pkgs.udev
+            pkgs.pkg-config
+        ];
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.mosquitto
+              pkgs.rustc
+              pkgs.cargo
+              
+              (pkgs.vscode-with-extensions.override {
+                vscode = pkgs.vscodium;
+                vscodeExtensions = (with pkgs.vscode-extensions; [
+                  rust-lang.rust-analyzer
+                  vadimcn.vscode-lldb
+                ]);
+              })
+            ] ++ deps;
+          };
 
-  };
+          packages = {
+            default = pkgs.rustPlatform.buildRustPackage {
+              name = "mqtt-light";
+              src = pkgs.lib.cleanSource ./.;
+              buildInputs = deps;
+              nativeBuildInputs = deps;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                # Allow dependencies to be fetched from git and avoid having to set the outputHashes manually
+                allowBuiltinFetchGit = true;
+              };
+              doCheck = false;
+            };
+          };
+
+        };
+      };
 }
